4c45b16503875c9aa4abf4bda2b8ea17b727b6fc
若云 <zyhdvlp@gmail.com>
2019-07-05 18:53:23 +0800

    添加 bridge

---
+++ weber-x5-jsbridge/README.md
@@ -0,0 +1,17 @@
+
+# WeBer jsBridge
+
+## H5开发体验
+
+h5 的使用方法和  ios 的 [WebViewJavascriptBridge](https://github.com/marcuswestin/WebViewJavascriptBridge) 库的一致，h5 在开发中只要实现一套代码就可以和 ios android 进行通信。
+
+## 使用
+
+```groovy
+implementation 'vip.ruoyun.webkit:weber-x5-jsbridge:1.0.2'
+```
+
+
+
+
+
---
+++ weber-x5-jsbridge/src/main/java/vip/ruoyun/webkit/x5/jsbridge/WeBerViewBridgeClient.java
@@ -0,0 +1,31 @@
+package vip.ruoyun.webkit.x5.jsbridge;
+
+import com.tencent.smtt.sdk.WebView;
+
+import vip.ruoyun.webkit.x5.WeBerViewClient;
+
+/**
+ * Created by ruoyun on 2019-07-05.
+ * Author:若云
+ * Mail:zyhdvlp@gmail.com
+ * Depiction:
+ */
+public class WeBerViewBridgeClient extends WeBerViewClient {
+
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+
+
+
+
+
+        return super.shouldOverrideUrlLoading(view, url);
+    }
+
+
+
+
+
+}
