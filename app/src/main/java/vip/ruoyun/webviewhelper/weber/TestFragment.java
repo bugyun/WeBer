0850c5447864f3c7a05abe5ea3c0d44fd23dec14
若云 <zyhdvlp@gmail.com>
2019-07-08 22:44:29 +0800

    修改 jsbridge 的代码

---
+++ app/src/main/java/vip/ruoyun/webviewhelper/weber/TestFragment.java
@@ -0,0 +1,42 @@
+package vip.ruoyun.webviewhelper.weber;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vip.ruoyun.webviewhelper.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TestFragment extends Fragment {
+
+
+    public TestFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
+    }
+}
